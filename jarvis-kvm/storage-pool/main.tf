44a1e355034526aabb5bd788396fd7321cdc488e
Rahmat Agung W <rahmat.agung.w@mail.ugm.ac.id>
2025-07-06 12:07:24 +0700

    add server for ceph storage (#3)

--- jarvis-kvm/storage-pool/main.tf
+++ jarvis-kvm/storage-pool/main.tf
@@ -17,3 +17,16 @@ resource "libvirt_volume" "fedora42" {
   pool   = libvirt_pool.default.name
   source = "https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
 }
+
+resource "libvirt_volume" "centos9" {
+  name   = "centos9"
+  pool   = libvirt_pool.default.name
+  source = "https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-GenericCloud-9-latest.x86_64.qcow2"
+}
+
+resource "libvirt_volume" "rocky9" {
+  name   = "rocky9"
+  pool   = libvirt_pool.default.name
+  source = "https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2"
+  
+}
--- jarvis-kvm/storage-pool/outputs.tf
+++ jarvis-kvm/storage-pool/outputs.tf
@@ -9,3 +9,9 @@ value = libvirt_volume.debian12.name
 output "image_fedora42" {
 value = libvirt_volume.fedora42.name 
 }
+output "image_centos9" {
+value = libvirt_volume.centos9.name
+}
+output "image_rocky9" {
+value = libvirt_volume.rocky9.name
+}
